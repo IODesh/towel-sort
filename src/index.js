ae8aac752ac95c31b38ab40f56c2394555299bd6
Igor Desh <desh_gambler@mail.ru>
2021-07-16 20:00:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 module.exports = function towelSort (matrix) {
     let result = [];
     let arr = matrix;
-    if (arr.toString().length !== 0) {
+    if (arr.length !== 0) {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
           let jReverse = i % 2 === 0 
